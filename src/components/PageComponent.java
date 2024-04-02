410a82cd9dc12bf3a527108e8ca7940a374685ac
Connor Luce <lucerc@rose-hulman.edu>
2024-04-02 10:43:05 -0400

    Added PageComonent Class

--- src/components/PageComponent.java
+++ src/components/PageComponent.java
@@ -1,20 +1,52 @@
 package components;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLEditorKit;
 
 public class PageComponent {
-	JPanel panel;
-	String fileName;
-	JFrame frame;
+	private JPanel panel;
+	private String fileName;
+	private String filePath;
+	private JFrame frame;
+	private String fileData;
+	private Path path;
+    private JEditorPane pane;
+	private HTMLEditorKit kit;
+	private Document doc;
 	/**
 	 * @param fileName
+	 * @param filePath
 	 * @param frame
 	 */
-	public PageComponent(String fileName, JFrame frame) {
+	public PageComponent(String fileName, String filePath, JFrame frame) {
+		this.filePath = filePath;
 		this.fileName = fileName;
 		this.frame = frame;
-		
+		this.path = Path.of("MrCanadaData\\Pages\\" + this.filePath + "\\" + this.fileName);
+		try {
+			this.fileData = Files.readString(this.path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.pane = new JEditorPane();
+		this.kit = new HTMLEditorKit();
+	    this.pane.setEditorKit(kit);
+	    this.doc = kit.createDefaultDocument();
+	    this.pane.setDocument(doc);
+        this.pane.setText(this.fileData);
+        this.panel.add(this.pane);
+	}
+	public void open() {
+		this.frame.add(this.panel);
+	}
+	public void close() {
+		this.frame.remove(this.panel);
 	}
 	
 }
