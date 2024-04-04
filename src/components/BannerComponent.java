588abf958c0da6c7cc87a9fd7ed7751b2b559eac
Connor Luce <lucerc@rose-hulman.edu>
2024-04-04 10:06:51 -0400

    changed bannerComponent

---
+++ src/components/BannerComponent.java
@@ -0,0 +1,17 @@
+package components;
+
+import javax.swing.JFrame;
+
+public class BannerComponent extends HTMLComponent {
+
+	/**
+	 * @param fileName
+	 * @param filePath
+	 * @param frame
+	 */
+	public BannerComponent(String fileName, String filePath, JFrame frame) {
+		super(fileName, filePath, frame);
+		// TODO Auto-generated constructor stub
+	}
+
+}
