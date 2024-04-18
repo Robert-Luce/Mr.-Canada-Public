29e32f1bae74f8894e2d5efa110e486b6833544b
jaraczlo <jaraczlo@rose-hulman.edu>
2024-04-18 10:37:16 -0400

    Javadocs comments

--- src/Trail.java
+++ src/Trail.java
@@ -3,11 +3,23 @@ public class Trail extends Attraction {
 
 	private boolean canUseBike;
 
+	/**
+	 * ensures: default constructor for the Attraction class
+	 * @param name				- String - the name of trail
+	 * @param city				- String - the city where trail is located
+	 * @param isAccessible		- boolean - whether or not it is wheel chair accessible
+	 * @param canUseBike		- boolean - whether or not a bike can be used on the trail
+	 * @param priceLevel		- String - how expensive it is to go on this trail
+	 */
 	public Trail(String name, String city, boolean isAccessible, boolean canUseBike, String priceLevel) {
 		super(name, city, isAccessible, priceLevel);
 		this.canUseBike = canUseBike;
 	}
 
+	/**
+	 * ensures: information of if a bike can be used on a trail
+	 * @return canUseBike
+	 */
 	public boolean isBikingAllowed() {
 		return canUseBike;
 	}
