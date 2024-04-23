9aaa5cb6537bb8eddf6c6bda1099a7ad9acbcf12
rhit-walindqg <walindqg@rose-hulman.edu>
2024-04-22 22:23:39 -0400

    got rid of overlap on landing page :)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 import Page.Page;
@@ -7,8 +9,9 @@ public class Main {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(null);
-		Page page = new Page(frame, "Montreal");
+		Page page = new Page(frame, "DestinationCatalog");
 		page.open();
+		frame.setSize(new Dimension(1450, 1000));
 		frame.setVisible(true);
 		frame.repaint();
 	}
