3fa07c37fe5854df69d9db1d3816e09752ec9666
rhit-walindqg <walindqg@rose-hulman.edu>
2024-04-26 11:18:02 -0400

    changed things

--- src/survey/SurveyPanel.java
+++ src/survey/SurveyPanel.java
@@ -1,11 +1,14 @@
 package survey;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JCheckBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import place.*;
@@ -13,10 +16,14 @@ import place.*;
 public class SurveyPanel extends JPanel implements ActionListener{
 	ArrayList<String> questionTitles;
 	ArrayList<ArrayList<String>> responses;
+	private JFrame frame;
 	
-	public SurveyPanel(ArrayList<String> questionTitles, ArrayList<ArrayList<String>> responses, ArrayList<Place> places) {
+	public SurveyPanel(JFrame frame, ArrayList<String> questionTitles, ArrayList<ArrayList<String>> responses, ArrayList<Place> places) {
+//		super(new BorderLayout());
+		this.frame = frame;
 		this.questionTitles = questionTitles;
 		this.responses = responses;
+		
 		
 		this.questionTitles.add("What city are you interested in?");
 		this.questionTitles.add("What type of cuisine are you looking for?");
@@ -30,10 +37,14 @@ public class SurveyPanel extends JPanel implements ActionListener{
 		this.responses.get(1).add("Moroccan");
 		this.responses.get(1).add("Canadian");
 		
+//		JTable table = new JTable();
 		for(int i = 0; i < this.questionTitles.size(); i++) {
 			JLabel label = new JLabel(this.questionTitles.get(i));
-			this.add(label, BorderLayout.SOUTH);
+			JPanel tempPanel = new JPanel();
+//			label.setLocation(200, 100*i);
+			tempPanel.add(label);
 			for(int j = 0; j < this.responses.get(i).size(); j++) {
+				JPanel tempPanelInner = new JPanel();
 				String text = this.responses.get(i).get(j);
 				JCheckBox checkbox = new JCheckBox();
 				checkbox.setText(text);
@@ -51,11 +62,13 @@ public class SurveyPanel extends JPanel implements ActionListener{
 							System.out.println(text + " is unchecked");
 						}
 					} });
-				this.add(checkbox, BorderLayout.NORTH);
-			
+				checkbox.setLocation(200, 100*i + 50*j);
+				checkbox.setPreferredSize(new Dimension(100, 25));
+				tempPanelInner.add(checkbox);
+				tempPanel.add(tempPanelInner);
 			}
+				this.add(tempPanel);
 		}
-		
 		
 	}
 
