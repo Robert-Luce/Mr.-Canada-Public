5790adb7a62a0e52b24edc4bd5709ed0eac9452d
Connor Luce <lucerc@rose-hulman.edu>
2024-05-02 12:09:27 -0400

    made changes

---
+++ src/Main2.java
@@ -0,0 +1,26 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import components.LoginComponent;
+import page.Page;
+
+public class Main2 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Mr. Canada");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setPreferredSize(new Dimension(1080, 1000));
+		panel.setLocation(0, 0);
+		LoginComponent login = new LoginComponent(panel);
+		login.open();
+		frame.setSize(new Dimension(1080, 1000));
+		JScrollPane scroll = new JScrollPane(panel);
+		frame.add(scroll);
+		frame.setVisible(true);
+		frame.repaint();
+	}
+}
