c4093ecd34966c132486ffd8d3ee1175ff9ebbe6
leonemm <leonemm@gmail.com>
2024-05-09 10:39:54 -0400

    m

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,11 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
-		panel.setPreferredSize(new Dimension(1080, 1000));
+		panel.setPreferredSize(new Dimension(1080, 1400));
 		panel.setLocation(0, 0);
 		Page page = new Page(panel, "DestinationCatalog");
 		page.open();
-		frame.setSize(new Dimension(1080, 1000));
+		frame.setSize(new Dimension(1080, 1400));
 		JScrollPane scroll = new JScrollPane(panel);
 		frame.add(scroll);
 		frame.setVisible(true);
